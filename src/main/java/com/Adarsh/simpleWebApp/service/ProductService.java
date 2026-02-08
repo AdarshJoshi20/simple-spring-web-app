ea55ef669ba68a51521ac084e759a8d462e51d2e
AdarshJoshi20 <adarshjoshi2018633@gmail.com>
2026-02-08 23:13:28 +0530

    update Product class and added ProductRepo

--- src/main/java/com/Adarsh/simpleWebApp/service/ProductService.java
+++ src/main/java/com/Adarsh/simpleWebApp/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.Adarsh.simpleWebApp.service;
 import com.Adarsh.simpleWebApp.controller.ProductController;
 import com.Adarsh.simpleWebApp.model.Product;
+import com.Adarsh.simpleWebApp.repository.ProductRepo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,83 +18,90 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ProductService {
 
     private final Product product;
+    @Autowired
+    ProductRepo repo;
+    
 	
-	List<Product> products = new ArrayList<>(Arrays.asList(
-			new Product(101, "iphone", 60000),
-			new Product(100, "laptop", 100000),
-			new Product(102, "cannon", 12321)));
+//	List<Product> products = new ArrayList<>(Arrays.asList(
+//			new Product(101, "iphone", 60000),
+//			new Product(100, "laptop", 100000),
+//			new Product(102, "cannon", 12321)));
 
     ProductService(Product product) {
         this.product = product;
     }
 
 	public List<Product> getProducts(){
-		return products;
+		// this findAll() method is coming from JpaRepository
+		//as we have extended this class from the interface ProductRepository.
+		return repo.findAll();
 	}
 
 	
 
-//	public Product getProductById(int prodId) {
-//		return products.stream() // converts the list (of products) into a Stream.
-//				.filter(p -> p.getProdId() == prodId) // goes through each product (p) one by one and keeps only that product whose prodId matches given Id.
-//				.findFirst().get();// from the filtered results, pick the first matching product
-//	
-//	}
-	
-	//Equivalent code without using Stream 
 	public Product getProductById(int prodId) {
-	    for (Product product : products) {
-	        if (product.getProdId() == prodId) {
-	            return product; // product found
-	        }
-	    }
-	    return null; // product not found
+		return repo.findById(prodId).orElse(new Product());
 	}
+	
+//	//Equivalent code without using Stream 
+//	public Product getProductById(int prodId) {
+//	    for (Product product : products) {
+//	        if (product.getProdId() == prodId) {
+//	            return product; // product found
+//	        }
+//	    }
+//	    return null; // product not found
+//	}
 	
 	
 	// Adding a new product is done by the client on the server.
 	// ie the client tells the controller that it wants to add this particular product.
 	public void addProduct(Product prod)
 	{
-		
-		products.add(new Product(105, "blanket", 800));
+		repo.save(prod);
 	}
 
 
 
 	public void updateProduct(Product prod) {
-		// TODO Auto-generated method stub
+//		// TODO Auto-generated method stub
+//		
+//		int index = 0;
+//		
+//		for(int i = 0; i<products.size(); i++)
+//		{
+//			if(products.get(i).getProdId() == prod.getProdId())
+//				index = i;
+//		}
+//		products.set(index, prod);
 		
-		int index = 0;
-		
-		for(int i = 0; i<products.size(); i++)
-		{
-			if(products.get(i).getProdId() == prod.getProdId())
-				index = i;
-		}
-		products.set(index, prod);
+		// this save function will check if data is present then save it only.
+		//or if data is already present then update the data.
+		repo.save(prod);
 		
 	}
 
 	public void deleteProduct(int prodId) {
 		// TODO Auto-generated method stub
 		
-		int index = 0;
+//		int index = 0;
+//		
+//		// PROBLEM of below code: if entered id is not found, it will delete the 
+//		// first item in the product list, which should not happen ideally.
+//		for(int i = 0; i<products.size(); i++)
+//		{
+//			// if product id of any ith product (from products list)
+//			//matches the id entered (in the url search query) then remove it
+//			
+//			if(products.get(i).getProdId() == prodId)
+//			{
+//				index = i;
+//			}
+//		}
+//		
+//		products.remove(index);
 		
-		// PROBLEM of below code: if entered id is not found, it will delete the 
-		// first item in the product list, which should not happen ideally.
-		for(int i = 0; i<products.size(); i++)
-		{
-			// if product id of any ith product (from products list)
-			//matches the id entered (in the url search query) then remove it
-			
-			if(products.get(i).getProdId() == prodId)
-			{
-				index = i;
-			}
-		}
-		
-		products.remove(index);
+		repo.deleteById(prodId);
 		
 	}
 
